5d5a9955dc2f006fb19791af627c4e7dd06be06b
Rohan Kumar Singh <rosingh3342@gmail.com>
2024-01-15 14:17:49 +0530

    Packages Added

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { ZoomVideoSdkProvider } from '@zoom/react-native-videosdk';
 
+import ZOOM_APP_CONFIG from './utils/constant';
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+  container: { flex: 1 },
 });
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ZoomVideoSdkProvider
-        config={{
-          appGroupId: 'group.test.sdk',
-          domain: 'zoom.us',
-          enableLog: true,
-        }}
-      >
+      <ZoomVideoSdkProvider config={ZOOM_APP_CONFIG}>
         <Text>ROHAN</Text>
       </ZoomVideoSdkProvider>
     </View>
---
+++ src/utils/constant.js
@@ -0,0 +1,7 @@
+export const ZOOM_APP_CONFIG = {
+  enableLog: true,
+  domain: 'zoom.us',
+  appGroupId: 'group.test.sdk',
+};
+export const ZOOM_APP_KEY = 'VJoDvaMPTkC-26MIM0mhSA';
+export const ZOOM_APP_SECRET = 'a5odvXUh2cHYF9QkhvXb1sXsVGo6B9XzeIEE';
