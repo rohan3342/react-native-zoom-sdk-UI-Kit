8cbbda1c3e78eb0911107fc41ad188fcdb8ee956
Rohan Kumar Singh <rosingh3342@gmail.com>
2024-01-15 15:12:52 +0530

    SVG Setup and Few Default SVGs added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { ZoomVideoSdkProvider } from '@zoom/react-native-videosdk';
 
+import { CameraOff } from './assets/SVG';
 import ZOOM_APP_CONFIG from './utils/constant';
-
 const styles = StyleSheet.create({
-  container: { flex: 1 },
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
 });
 
 export default function App() {
   return (
     <View style={styles.container}>
       <ZoomVideoSdkProvider config={ZOOM_APP_CONFIG}>
-        <Text>ROHAN</Text>
+        <CameraOff />
       </ZoomVideoSdkProvider>
     </View>
   );
