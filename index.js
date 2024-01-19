701efe5c0ae2a85f9e5462c7ffd9c63ff13c0362
Rohan Kumar Singh <rosingh3342@gmail.com>
2024-01-19 21:48:22 +0530

    Revert "Update index.js"
    This reverts commit 1d9e10cb64519a47bd9a90881a0b05c823b58763.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// import 'react-native-gesture-handler';
-// import { AppRegistry } from 'react-native';
+import 'react-native-gesture-handler';
+import { AppRegistry } from 'react-native';
 
 import App from './src/App';
-// import { name as appName } from './app.json';
+import { name as appName } from './app.json';
 
-export default App;
-
-// AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App);
